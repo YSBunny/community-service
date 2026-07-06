c82ce97262a82bf0bacbfcbca2554394d0f1c366
seul_bunny <yun0836@naver.com>
2026-07-06 20:47:53 +0900

    feat: UserDetailsService 구현

---
+++ server/src/main/java/io/github/ysbunny/community/global/security/CustomUserDetailsService.java
@@ -0,0 +1,33 @@
+package io.github.ysbunny.community.global.security;
+
+import io.github.ysbunny.community.user.domain.User;
+import io.github.ysbunny.community.user.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("user not found with email: " + username));
+
+        // Spring Security의 UserDetails 객체로 변환
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(),
+                user.getPassword(),
+                Collections.emptyList() // 권한 비워둠
+        );
+    }
+}
