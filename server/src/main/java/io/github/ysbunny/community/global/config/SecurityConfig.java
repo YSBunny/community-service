264e6031ba0aca5c2253df52ca5b4dcf28e99251
seul_bunny <yun0836@naver.com>
2026-07-08 01:00:38 +0900

    feat: AuthenticationManager 설정

--- server/src/main/java/io/github/ysbunny/community/global/config/SecurityConfig.java
+++ server/src/main/java/io/github/ysbunny/community/global/config/SecurityConfig.java
@@ -5,6 +5,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -56,5 +58,10 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder() {
         // 비밀번호 암호화를 위한 PasswordEncoder 빈 등록
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
     }
 }
