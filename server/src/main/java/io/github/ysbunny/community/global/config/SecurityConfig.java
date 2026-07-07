0d081d78cf7ecf8816d8daa6e6b0948da4d3a103
seul_bunny <yun0836@naver.com>
2026-07-07 17:45:17 +0900

    feat: CSRF/로그인 비활성화, 세션 정책 설정

--- server/src/main/java/io/github/ysbunny/community/global/config/SecurityConfig.java
+++ server/src/main/java/io/github/ysbunny/community/global/config/SecurityConfig.java
@@ -1,21 +1,39 @@
 package io.github.ysbunny.community.global.config;
 
+import io.github.ysbunny.community.global.security.JwtFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @EnableWebSecurity // 스프링 시큐리티 필터 체인 활성화
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtFilter jwtFilter;
+    private final CorsFilter corsFilter;    // 별도 설정한 CORS 필터
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+                // 1. CSRF 비활성화
+                .csrf(csrf -> csrf.disable())
+                // 2. 폼 로그인, Basic 인증 비활성화
+                .formLogin(formLogin -> formLogin.disable())
+                .httpBasic(httpBasic -> httpBasic.disable())
+                // 3. 세션 설정: STATELESS
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                // 4. 권한 설정
                 .authorizeHttpRequests(auth -> auth
                         // 회원가입과 로그인은 로그인 하지 않은 사용자 전용
                         .requestMatchers(HttpMethod.POST, "/api/users").anonymous()
