8a76e0f1f720ff9c6ba6ca725e81521d8bb41cd4
seul_bunny <yun0836@naver.com>
2026-07-14 20:32:28 +0900

    feat: 비밀번호 수정 페이지 뒤로가기 버튼 누르면 게시글 목록 조회 페이지로 이동

--- client/js/password-edit.js
+++ client/js/password-edit.js
@@ -1,8 +1,14 @@
 import { updatePassword } from "./api/userApi.js";
 
 document.addEventListener("DOMContentLoaded", async function () {
+    const backButton = document.querySelector("#backButton");
+
     const profileMenuButton = document.querySelector("#profileMenuButton");
     const profileDropdown = document.querySelector("#profileDropdown");
+
+    backButton.addEventListener("click", () => {
+        window.location.href = `./posts.html`;
+    });
 
     // 2. 프로필 메뉴 누르면 드롭다운 보이게
     profileMenuButton.addEventListener("click", () => {
