292f0665cf7e435869b69ade8b7c984d12b84e65
seul_bunny <yun0836@naver.com>
2026-07-14 20:17:17 +0900

    feat: 게시글 상세 조회 페이지 뒤로가기 버튼 누르면 게시글 목록 페이지로 이동

--- client/js/post-detail.js
+++ client/js/post-detail.js
@@ -3,8 +3,14 @@ import { createComment, getComments, updateComment, deleteComment } from './api/
 
 // 0. HTML이 다 로드된 뒤 이벤트 리스너를 등록
 document.addEventListener("DOMContentLoaded", async function () {
+    const backButton = document.querySelector("#backButton");
+
     const profileMenuButton = document.querySelector("#profileMenuButton");
     const profileDropdown = document.querySelector("#profileDropdown");
+
+    backButton.addEventListener("click", () => {
+        window.location.href = `./posts.html`;
+    });
 
     // 2. 프로필 메뉴 누르면 드롭다운 보이게
     profileMenuButton.addEventListener("click", () => {
