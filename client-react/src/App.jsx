aecdd971effcd88f0c4c4810b40a428014cd2def
seul_bunny <yun0836@naver.com>
2026-07-26 20:59:07 +0900

    feat: LoginPage 라우트 설정

--- client-react/src/App.jsx
+++ client-react/src/App.jsx
@@ -1,9 +1,23 @@
+import { Navigate, Route, Routes } from "react-router";
+
+import LoginPage from "./pages/LoginPage.jsx";
+
 import "./App.css";
 
 function App() {
   return (
     <div className="app">
-      <h1>냐르륵</h1>
+      <Routes>
+        <Route
+          path="/"
+          element={<Navigate to="/posts" replace />}
+        />
+
+        <Route
+          path="/login"
+          element={<LoginPage />}
+        />
+      </Routes>
     </div>
   );
 }
