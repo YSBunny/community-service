f0032898ba6062a88a1c1e08654ec9b8c99eb008
seul_bunny <yun0836@naver.com>
2026-07-28 14:42:35 +0900

    refactor: SignupPage CSS 이름 수정

--- client-react/src/pages/SignupPage.jsx
+++ client-react/src/pages/SignupPage.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { Link } from "react-router";
 
 import Header from "../components/Header";
-import "../styles/signup.css";
+import "../styles/SignupPage.css";
 
 // 이메일 검증 규칙
 const EMAIL_PATTERN = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
