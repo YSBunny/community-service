f913f1bb2085ad429b812dc6001a768e9c9683fd
seul_bunny <yun0836@naver.com>
2026-07-30 20:11:10 +0900

    feat: 게시글 작성 및 수정 API 생성

--- client-react/src/api/postApi.js
+++ client-react/src/api/postApi.js
@@ -1,8 +1,45 @@
 import { request } from "./http.js";
 
+function createPostFormData({
+    title,
+    content,
+    postImage
+}) {
+    const formData = new FormData();
+
+    formData.append("title", title);
+    formData.append("content", content);
+
+    if (postImage) {
+        formData.append("postImage", postImage);
+    }
+
+    return formData;
+}
+
+// 게시글 작성
+export function createPost(postData) {
+    const formData = createPostFormData(postData);
+
+    return request("/posts", {
+        method: "POST",
+        body: formData
+    });
+}
+
 // 게시글 목록
 export function getPosts() {
     return request("/posts", {
         method: "GET"
+    });
+}
+
+// 게시글 수정
+export function updatePost(postId, postData) {
+    const formData = createPostFormData(postData);
+
+    return request(`/posts/${postId}`, {
+        method: "PATCH",
+        body: formData
     });
 }
