506439781da8843846952c5d7c459f884e163896
seul_bunny <yun0836@naver.com>
2026-07-31 14:31:33 +0900

    feat: 게시글 상세 조회 및 삭제 API 생성

--- client-react/src/api/postApi.js
+++ client-react/src/api/postApi.js
@@ -27,9 +27,16 @@ export function createPost(postData) {
     });
 }
 
-// 게시글 목록
+// 게시글 목록 조회
 export function getPosts() {
     return request("/posts", {
+        method: "GET"
+    });
+}
+
+// 게시글 상세 조회
+export function getPost(postId) {
+    return request(`/posts/${postId}`, {
         method: "GET"
     });
 }
@@ -41,5 +48,12 @@ export function updatePost(postId, postData) {
     return request(`/posts/${postId}`, {
         method: "PATCH",
         body: formData
+    });
+}
+
+// 게시글 삭제
+export function deletePost(postId) {
+    return request(`/posts/${postId}`, {
+        method: "DELETE"
     });
 }
