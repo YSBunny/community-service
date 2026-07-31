2e2f27f6d17f1687568da47a19b39c6568e60456
seul_bunny <yun0836@naver.com>
2026-08-01 06:22:54 +0900

    style: commentApi 코드 포맷팅 및 들여쓰기 정돈

--- client-react/src/api/commentApi.js
+++ client-react/src/api/commentApi.js
@@ -2,31 +2,30 @@ import { request } from "./http.js";
 
 // 댓글 등록
 export function createComment(postId, commentData) {
-    return request(`/posts/${postId}/comments`, {
-        method: "POST",
-        body: JSON.stringify(commentData)
-    });
+  return request(`/posts/${postId}/comments`, {
+    method: "POST",
+    body: JSON.stringify(commentData)
+  });
 }
 
 // 댓글 목록 조회
 export function getComments(postId) {
-    return request(`/posts/${postId}/comments`, {
-        method: "GET"
-    });
+  return request(`/posts/${postId}/comments`, {
+    method: "GET"
+  });
 }
 
 // 댓글 수정
 export function updateComment(postId, commentId, commentData) {
-    return request(`/posts/${postId}/comments/${commentId}`, {
-        method: "PATCH",
-        body: JSON.stringify(commentData)
-    });
+  return request(`/posts/${postId}/comments/${commentId}`, {
+    method: "PATCH",
+    body: JSON.stringify(commentData)
+  });
 }
 
 // 댓글 삭제
 export function deleteComment(postId, commentId) {
-    return request(`/posts/${postId}/comments/${commentId}`, {
-        method: "DELETE"
-    });
+  return request(`/posts/${postId}/comments/${commentId}`, {
+    method: "DELETE"
+  });
 }
-
