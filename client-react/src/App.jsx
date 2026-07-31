96b7d2df5be172729ea29bd57c3575b44ee9ec89
seul_bunny <yun0836@naver.com>
2026-08-01 07:41:25 +0900

    feat: ProtectedRoute 인증 가드 구현 및 라우팅 구조 개선
    - localStorage의 accessToken 존재 여부를 확인하는 ProtectedRoute 컴포넌트 추가
    - 인증 필요 페이지(게시글/프로필 관리)를 ProtectedRoute 하위 중첩 라우트로 보호
    - 루트 경로(/) 접근 시 로그인 상태에 따라 /posts 또는 /login으로 다이나믹 리다이렉트
    - 존재하지 않는 URL(*) 접근 시 루트 경로로 리다이렉트 처리
    - 임포트 구문 알파벳순 정돈

--- client-react/src/App.jsx
+++ client-react/src/App.jsx
@@ -1,64 +1,51 @@
-import { Navigate, Route, Routes } from "react-router";
+import { Navigate, Outlet, Route, Routes } from "react-router";
 
 import LoginPage from "./pages/LoginPage.jsx";
-import SignupPage from "./pages/SignupPage.jsx";
-import PostListPage from "./pages/PostListPage.jsx";
-import PostEditPage from "./pages/PostEditPage.jsx";
-import PostDetailPage from "./pages/PostDetailPage.jsx";
-import ProfileEditPage from "./pages/ProfileEditPage.jsx";
 import PasswordEditPage from "./pages/PasswordEditPage.jsx";
+import PostCreatePage from "./pages/PostCreatePage.jsx";
+import PostDetailPage from "./pages/PostDetailPage.jsx";
+import PostEditPage from "./pages/PostEditPage.jsx";
+import PostListPage from "./pages/PostListPage.jsx";
+import ProfileEditPage from "./pages/ProfileEditPage.jsx";
+import SignupPage from "./pages/SignupPage.jsx";
 
 import "./App.css";
-import PostCreatePage from "./pages/PostCreatePage.jsx";
+
+// 로그인한 사용자만 자식 Route를 보여주는 컴포넌트
+function ProtectedRoute() {
+  const accessToken = localStorage.getItem("accessToken");
+
+  if (!accessToken) {
+    return <Navigate to="/login" replace />;
+  }
+
+  return <Outlet />;
+}
 
 function App() {
+  const accessToken = localStorage.getItem("accessToken");
+
   return (
     <div className="app">
       <Routes>
         <Route
           path="/"
-          element={<Navigate to="/posts" replace />}
+          element={<Navigate to={accessToken ? "/posts" : "/login"} replace />}
         />
 
-        <Route
-          path="/login"
-          element={<LoginPage />}
-        />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
 
-        <Route
-          path="/signup"
-          element={<SignupPage />}
-        />
+        <Route element={<ProtectedRoute />}>
+          <Route path="/posts" element={<PostListPage />} />
+          <Route path="/posts/new" element={<PostCreatePage />} />
+          <Route path="/posts/:postId" element={<PostDetailPage />} />
+          <Route path="/posts/:postId/edit" element={<PostEditPage />} />
+          <Route path="/users/:userId/edit" element={<ProfileEditPage />} />
+          <Route path="/users/:userId/password" element={<PasswordEditPage />} />
+        </Route>
 
-        <Route
-          path="/users/:userId/edit"
-          element={<ProfileEditPage />}
-        />
-
-        <Route
-          path="/users/:userId/password"
-          element={<PasswordEditPage />}
-        />
-        
-        <Route
-          path="/posts"
-          element={<PostListPage />}
-        />
-
-        <Route
-          path="/posts/new"
-          element={<PostCreatePage />}
-        />
-
-        <Route
-          path="/posts/:postId"
-          element={<PostDetailPage />}
-        />
-
-        <Route
-          path="/posts/:postId/edit"
-          element={<PostEditPage />}
-        />
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </div>
   );
